56449565e519d1d6512881659585ee616e726281
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-10 23:05:03 +0300

    last

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- second TABLE
+-- second tableee
 CREATE TABLE IF NOT EXISTS second_table(
   id INT,
   name VARCHAR(256),
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS second_table(
 INSERT INTO second_table VALUES(1,  “John”, 10);  
 INSERT INTO second_table VALUES(2,  “Alex”, 3);
 INSERT INTO second_table VALUES( 3, “Bob”, 14);
-  INSERT INTO second_table VALUES(4,  “George”,  8);
+INSERT INTO second_table VALUES(4,  “George”,  8);
