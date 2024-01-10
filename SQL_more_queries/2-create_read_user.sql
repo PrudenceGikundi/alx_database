b86c67368ae3192b50c36d74dc42159f6d724791
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-11 00:09:06 +0300

    COM

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Using select STATEMENT
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
-GRANT ALL PRIVILEGES ON
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
